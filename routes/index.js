d42b1154e13d61110453703e3927e58611ece71f
srj004 <srj004@ucsd.edu>
2014-02-06 11:22:42 -0800

    putting it together lab

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	//res.render('index');
+
+	res.render('index', {'data' : data});
+
 };
